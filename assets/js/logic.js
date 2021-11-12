a06a04450266a519d351bd6626cddcb2094bd4b1
Hunter <no@no.com>
2021-11-12 12:25:29 -0500

    update answer btn

---
+++ assets/js/logic.js
@@ -0,0 +1,172 @@
+// variables to keep track of quiz state
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+// variables to reference DOM elements
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var wrongEl = document.getElementById("wrong");
+var correctEl = document.getElementById("correct");
+
+// sound effects
+var sfxRight = new Audio("assets/sfx/correct.wav");
+var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+
+function startQuiz() {
+  // hide start screen
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+
+  // un-hide questions section
+  questionsEl.removeAttribute("class");
+
+  // start timer
+  timerId = setInterval(clockTick, 1000);
+
+  // show starting time
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+
+function getQuestion() {
+  // get current question object from array
+  var currentQuestion = questions[currentQuestionIndex];
+
+  // update title with current question
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+  // clear out any old question choices
+  choicesEl.innerHTML = "";
+
+  // loop over choices
+  currentQuestion.choices.forEach(function (choice, i) {
+    // create new button for each choice
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    // attach click event listener to each choice
+    choiceNode.onclick = questionClick;
+
+    // display on the page
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+function questionClick() {
+  // check if user guessed wrong
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    // penalize time
+    time -= 15;
+
+    if (time < 0) {
+      time = 0;
+    }
+
+    // display new time on page
+    timerEl.textContent = time;
+
+    // play "wrong" sound effect
+    // sfxWrong.play();
+
+    wrongEl.textContent = "Wrong!";
+    // flash right/wrong feedback on page for half a second
+    wrongEl.setAttribute("class", "wrong");
+    setTimeout(function () {
+      wrongEl.setAttribute("class", "wrong hide");
+    }, 1000);
+  } else {
+    correctEl.textContent = "Correctttttt!";
+    // flash right/wrong feedback on page for half a second
+    correctEl.setAttribute("class", "correct");
+    setTimeout(function () {
+      correctEl.setAttribute("class", "correct hide");
+    }, 1000);
+
+  }
+
+  currentQuestionIndex++;
+
+  // check if we've run out of questions
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+function quizEnd() {
+  // stop timer
+  clearInterval(timerId);
+
+  // show end screen
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
+
+  // show final score
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
+
+  // hide questions section
+  questionsEl.setAttribute("class", "hide");
+}
+
+function clockTick() {
+  // update time
+  time--;
+  timerEl.textContent = time;
+
+  // check if user ran out of time
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+
+function saveHighscore() {
+  // get value of input box
+  var initials = initialsEl.value.trim();
+
+  // make sure value wasn't empty
+  if (initials !== "") {
+    // get saved scores from localstorage, or if not any, set to empty array
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // format new score object for current user
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+
+    // save to localstorage
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    // redirect to next page
+    window.location.href = "highscores.html";
+  }
+}
+
+function checkForEnter(event) {
+  // "13" represents the enter key
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+// user clicks button to submit initials
+submitBtn.onclick = saveHighscore;
+
+// user clicks button to start quiz
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
---
+++ assets/js/questions.js
@@ -0,0 +1,89 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+        title: "How many legs does a spider have?",
+        choices: [
+            "8", 
+            "9", 
+            "6", 
+            "4"],
+        answer: "8"
+    },
+    {
+        title: "What is the name of the toy cowboy in Toy Story.",
+        choices: [
+            "Woody",
+            "Buzz", 
+            "Potatoe", 
+            "Mike"],
+        answer: "Woody"
+    },
+    {
+        title: "What is the color of an emerald?",
+        choices: [
+            "Yellow",
+            "Orange",
+            "Green",
+            "Pink"
+        ],
+        answer: "Green"
+    },
+    {
+        title:
+            "What is something you hit with a hammer?.",
+        choices: ["Car", 
+            "Window", 
+            "A Nail", 
+            "Wood"],
+        answer: "A Nail"
+    },
+    {
+        title:
+            "What’s the name of a place you go to see lots of animals?",
+        choices: ["Tinger King", 
+            "Zoo", 
+            "Bar", 
+            "Online"],
+        answer: "Zoo"
+    },
+    {
+        title: "Whose nose grew longer every time he lied?",
+        choices: [
+            "Pinocchio",
+            "Batman",
+            "Woody",
+            "The person next to you"
+        ],
+        answer: "Pinocchio"
+    },
+    {
+        title: " What is the name of the fairy in Peter Pan?",
+        choices: [
+            "Pam",
+            "Tracy",
+            "Lisa",
+            "Tinkerbell"
+        ],
+        answer: "Tinkerbell"
+    },
+    {
+        title: "If you freeze water, what do you get?",
+        choices: [
+            "Ice",
+            "Ice Cream",
+            "Jello",
+            "Pan Cake"
+        ],
+        answer: "Ice"
+    },
+    {
+        title: "What colors are the stars on the American flag?",
+        choices: [
+            "Red",
+            "White",
+            "Blue",
+            "Red and white"
+        ],
+        answer: "White"
+    }
+];
